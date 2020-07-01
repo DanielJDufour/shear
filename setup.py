7eab1e83632277b89a2aa03873b516710d0396ec
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-30 22:24:34 -0400

    updated setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 from os import path
+from re import sub
 
 this_directory = path.abspath(path.dirname(__file__))
 
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
+
+# remove emojiis
+long_description = sub(":[a-z]+:", "- ", long_description)
+
+# replace html top
+long_description = "# Shear\nTrim Excess Quotes from your Strings\n" + long_description[long_description.index("#"):]
 
 setup(
   name = 'shear',
@@ -12,7 +19,7 @@ setup(
   entry_points={
       'console_scripts': ['shear=shear.__init__:main'],
   },
-  version = "0.0.3",
+  version = "0.0.4",
   description = '🐑 Shear: Trim Excess Quotes from your Strings in Python and on the Command Line',
   long_description = long_description,
   long_description_content_type='text/markdown',
